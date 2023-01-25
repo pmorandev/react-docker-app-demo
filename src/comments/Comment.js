266cc175b76c7296929e280973f9c4e67f948226
Pedro Moran <p.morandev@gmail.com>
2023-01-24 21:16:47 -0500

    First commit

---
+++ src/comments/Comment.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Comment extends React.Component{
+    render(){
+        return(
+            <article>
+                <p>Pedro Moran</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet urna sed risus laoreet condimentum. Curabitur condimentum diam sit amet mauris lacinia commodo. Mauris velit ante, sagittis at egestas in, iaculis sit amet mauris. Aenean pharetra tempus fringilla. Suspendisse scelerisque a turpis sed rhoncus. Fusce convallis urna metus, vitae convallis dui tincidunt a. Phasellus dictum tempus posuere. Praesent quis ex ac leo pulvinar sodales. Quisque dignissim libero eget neque aliquet, in accumsan dolor ornare.</p>
+            </article>
+        )
+    }
+}
